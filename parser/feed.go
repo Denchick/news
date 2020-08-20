579024bc8aba560b3f1c97c0fc500c0268bab9ca
Volkov Denis <volkov.denis.dev@yandex.ru>
2020-08-20 20:36:41 +0500

    save more readable description from feed

--- parser/feed.go
+++ parser/feed.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/PuerkitoBio/goquery"
 	"github.com/denchick/news/database"
 	"github.com/mmcdole/gofeed"
+	"strings"
 )
 
 func getFeeds() []string {
@@ -15,10 +17,20 @@ func parseFeed(feedUrl string) (articlesArr []database.Article) {
 	for _, item := range feed.Items {
 		article := database.Article{
 			Title: item.Title,
-			Description: item.Description,
+			Description: getCleanedDescription(item.Description, 500),
 			Link: item.Link,
 		}
 		articlesArr = append(articlesArr, article)
 	}
 	return
+}
+
+func getCleanedDescription(text string, limit int) (result string) {
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(text))
+	text = strings.TrimSpace(doc.Text())
+	runes := []rune(text)
+	if len(runes) >= limit {
+		return string(runes[:limit]) + "..."
+	}
+	return text
 }
