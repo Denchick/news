0c7f497378989fe71442e1d5e418bee18171cdbf
Volkov Denis <volkov.denis.dev@yandex.ru>
2020-08-21 01:45:43 +0500

    remove line endings in description

--- parser/utils.go
+++ parser/utils.go
@@ -17,6 +17,7 @@ func createArticle(title string, description string, link string) database.Artic
 func getCleanedDescription(text string, limit int) (result string) { // TODO replace /n to whitespace
 	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(text))
 	text = strings.TrimSpace(doc.Text())
+	text = strings.ReplaceAll(text, "\n", " ")
 	runes := []rune(text)
 	if len(runes) >= limit {
 		return string(runes[:limit]) + "..."
