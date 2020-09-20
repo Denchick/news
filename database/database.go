c97903c5e3d98403776e20c870e88ee0ba4b0862
Volkov Denis <volkov.denis.dev@yandex.ru>
2020-09-20 18:29:06 +0500

    add docker-compose for parser and server

--- database/database.go
+++ database/database.go
@@ -1,15 +1,15 @@
 package database
 
 import (
+	"log"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
 )
 
 func Connect() (*gorm.DB, error) {
-	// Так хранить не хорошо
-	dsn := "host=rogue.db.elephantsql.com port=5432 user=acumlegw dbname=acumlegw password=2ZbAnECs3Lfdc9k9sbpRWzcCOHd1mo97"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
 	if err != nil {
 		log.Fatal("failed to connect database")
 		return nil, err
