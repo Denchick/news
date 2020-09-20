d371588b10cd1ed1866b2d54dddb772414bfd87e
Volkov Denis <volkov.denis.dev@yandex.ru>
2020-09-20 17:08:12 +0500

    server works with docker

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ WORKDIR /app
 ## Add this go mod download command to pull in any dependencies
 RUN go mod download
 ## Our project will now successfully build with the necessary go libraries included.
-RUN go build -o main .
+RUN go build -o ./server/main ./server
 ## Our start command which kicks off
 ## our newly created binary executable
-CMD ["/app/main"]
+WORKDIR /app/server
+CMD ["/app/server/main"]
--- server/main.go
+++ server/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/denchick/news/server/controller"
-	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
+
+	"github.com/denchick/news/server/controller"
+	"github.com/julienschmidt/httprouter"
 )
 
 func createRouter() (router *httprouter.Router) {
@@ -20,7 +21,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	addr := "localhost:4444"
+	addr := ":8081"
 	log.Printf("run server on %s", addr)
 	err = http.ListenAndServe(addr, createRouter())
 	if err != nil {
